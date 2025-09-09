fcc739f9ce36467ea7ca624c6d8763f5ac81d6a7
IlyesAllouche <32812042+IlyesAllouche@users.noreply.github.com>
2025-09-09 22:36:02 +0200

    Create ransom.py

---
+++ ransom.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3 
+import sys
+import os
+
+f = open(sys.argv[1], "x")
+
+with open(sys.argv[1], "w") as f:
+        f.write("oueoueoueeeee")
+
+with open(sys.argv[1]) as f:
+        print("Après modifications : ", f.read())
+
+print("Suppression...")
+
+os.remove(sys.argv[1])
+
