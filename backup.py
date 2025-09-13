ab4f534fc2855d109020fd5ff58141d55d4d5530
IlyesAllouche <alloucheil@outlook.fr>
2025-09-13 07:43:14 -0500

    sshpass

--- backup.py
+++ backup.py
@@ -4,13 +4,31 @@ import requests
 import os
 import subprocess
 import sys
+import shutil
 
 print("------Files found------")
 dir_path = sys.argv[1]
+
+#install sshpass
+def has_sshpass():
+	return shutil.which("sshpass") is not None
+
+if has_sshpass():
+	print("sshpass already installed")
+else:
+	cmd = "sudo apt install sshpass"
+	subprocess.run(cmd, shell=True)
+
+#compress the whole directory to send it
 archive = "export.tar.gz"
 cmd = "tar -zcvf " + archive + " " + dir_path
 subprocess.run(cmd, shell=True)
+
+#send all files by scp wherever you want (change password)
 print("------Export------")
-cmd = "scp -o StrictHostKeyChecking=no " + archive + " ilyes@127.0.0.1:~/Desktop/receive"
+cmd = "sshpass -p 'ilyes' scp -o StrictHostKeyChecking=no " + archive + " ilyes@127.0.0.1:~/Desktop/receive"
 subprocess.run(cmd, shell=True)
+print("  [+] Files exported")
+
+#delete archive with all clear files
 os.remove(archive)
